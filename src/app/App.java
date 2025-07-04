5f10078e5173cdd33464fb4f70343976b0c3dba3
Mohamed Khaled <91966527+Mohamedkhaled2310@users.noreply.github.com>
2025-07-05 02:26:48 +0300

    first commit

---
+++ src/app/App.java
@@ -0,0 +1,20 @@
+package app;
+import model.*;
+import service.Checkout;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Product cheese = new ShippingProduct("Cheese", 100, 10, 0.4);
+        Product biscuits = new PerishableProduct("Biscuits", 50, 10);
+        Product mobile = new NonPerishableProduct("Mobile", 200, 10);
+
+        Customer customer = new Customer("Mohamed Khaled", 500);
+
+        Cart cart = new Cart();
+        cart.add(cheese, 2);
+        cart.add(biscuits, 1);
+        cart.add(mobile, 1);
+
+        Checkout.processCheckout(customer, cart);
+    }
+}
